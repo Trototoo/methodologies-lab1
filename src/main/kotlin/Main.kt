76fb5215391b4e4a801cc6b08b0192bbae84ce90
Олександр <sa44ko@gmail.com>
2023-03-02 11:16:15 +0200

    Added function to readNumber from user in interactive mode.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,21 @@
+import java.lang.NumberFormatException
+
 fun main(args: Array<String>) {
     println("Hello World!")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
+}
+
+private fun readNumber(string: String): Double {
+    while (true) {
+        print(string)
+        val input = readln()
+        try {
+            return input.toDouble()
+        } catch (e: NumberFormatException) {
+            println("Error. Expected a valid real number, got $input instead")
+        }
+    }
 }
